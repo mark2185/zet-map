4e1c9282e16ae8b5a3de35cb54f05040b32ad5dc
Luka Markušić <markusicluka@gmail.com>
2025-03-30 11:08:30 +0200

    Add direction to every vehicle

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/MobilityData/gtfs-realtime-bindings/golang/gtfs"
 	"google.golang.org/protobuf/proto"
@@ -17,7 +19,17 @@ type Vehicle struct {
 	Latitude  float32 `json:"lat"`
 	Longitude float32 `json:"lon"`
 	RouteID   string  `json:"route_id"`
+	Direction string  `json:"direction"`
 }
+
+type RouteID string
+type TripID string
+
+type TripDirection map[TripID]string
+
+type RouteTrips map[RouteID]TripDirection
+
+var routeDirections = RouteTrips{}
 
 func fetchGTFSRealTime(url string) (*gtfs.FeedMessage, error) {
 	resp, err := http.Get(url)
@@ -43,14 +55,25 @@ func fetchGTFSRealTime(url string) (*gtfs.FeedMessage, error) {
 	return feed, nil
 }
 
+func getDirection(route_id RouteID, trip_id TripID) string {
+	return routeDirections[route_id][trip_id]
+}
+
 func getAllVehicles(feed *gtfs.FeedMessage) []Vehicle {
 	var vehicles []Vehicle
 	for _, entity := range feed.Entity {
 		if entity.Vehicle != nil {
+			routeID := RouteID(*entity.Vehicle.Trip.RouteId)
+			tripID := TripID(*entity.Vehicle.Trip.TripId)
+			directionIcon := ">"
+			if getDirection(routeID, tripID) != "0" {
+				directionIcon = "<"
+			}
 			vehicles = append(vehicles, Vehicle{
 				Latitude:  *entity.Vehicle.Position.Latitude,
 				Longitude: *entity.Vehicle.Position.Longitude,
 				RouteID:   *entity.Vehicle.Trip.RouteId,
+				Direction: directionIcon,
 			})
 		}
 	}
@@ -76,10 +99,36 @@ func mapHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	tripsFile, err := os.Open("./data/trips.txt")
+	if err != nil {
+		log.Fatalf("Could not open trips info: %v\n", err)
+	}
+	defer tripsFile.Close()
+
+	tripsReader := csv.NewReader(tripsFile)
+
+	_, err = tripsReader.Read() // read the header
+	if err != nil {
+		log.Fatalf("Could not parse CSV: %v\n", err)
+	}
+
+	rawCSVdata, _ := tripsReader.ReadAll() // read the rest
+
+	for _, row := range rawCSVdata {
+		rid := RouteID(row[0])
+		tid := TripID(row[2])
+		dir := row[5]
+		if _, exists := routeDirections[rid]; !exists {
+			routeDirections[rid] = TripDirection{}
+		}
+
+		routeDirections[rid][tid] = dir
+	}
+
 	http.HandleFunc("/", mapHandler)
 	http.HandleFunc("/vehicles", vehicleHandler)
 
-	fmt.Println("Server running on port 8080")
+	log.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 
 	// if feed.Header.Timestamp != nil {
